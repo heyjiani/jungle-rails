e7ef233daa3f137dd88754b8e482a33609b995d2
Jiani Cao <jianicakes@gmail.com>
2022-03-15 12:35:35 -0700

    add error display for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      redirect_to signup_path(user_errors: user.errors.full_messages)
     end
   end
 
